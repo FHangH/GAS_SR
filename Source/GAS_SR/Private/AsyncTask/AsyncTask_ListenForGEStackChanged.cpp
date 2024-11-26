aac1d68c9378fa8e03187c4b898304188e4f2773
FHangH <752972182@qq.com>
2024-11-26 17:19:07 +0800

    Add: AsyncTask_ListenForGEStackChanged cpp file;

---
+++ Source/GAS_SR/Private/AsyncTask/AsyncTask_ListenForGEStackChanged.cpp
@@ -0,0 +1,78 @@
+﻿#include "AsyncTask/AsyncTask_ListenForGEStackChanged.h"
+#include "AbilitySystemComponent.h"
+
+UAsyncTask_ListenForGEStackChanged* UAsyncTask_ListenForGEStackChanged::ListenForGEStackChanged(
+	UObject* InWorldContextObject, UAbilitySystemComponent* InASComponent, FGameplayTag InEffectGameplayTag)
+{
+	AsyncTask = NewObject<UAsyncTask_ListenForGEStackChanged>();
+	if (AsyncTask)
+	{
+		AsyncTask->WorldContextObject = InWorldContextObject;
+		AsyncTask->ASComponent = InASComponent;
+		AsyncTask->EffectGameplayTag = InEffectGameplayTag;
+		return AsyncTask;
+	}
+	return nullptr;
+}
+
+void UAsyncTask_ListenForGEStackChanged::Activate()
+{
+	AddToRoot();
+	RegisterWithGameInstance(WorldContextObject);
+
+	if (!ASComponent || !EffectGameplayTag.IsValid())
+	{
+		EndTask();
+		return;
+	}
+
+	ASComponent->OnActiveGameplayEffectAddedDelegateToSelf.AddUObject(this, &ThisClass::OnActiveGameplayEffectAddedCallback);
+	ASComponent->OnAnyGameplayEffectRemovedDelegate().AddUObject(this, &ThisClass::OnRemoveGameplayEffectCallback);
+}
+
+void UAsyncTask_ListenForGEStackChanged::OnActiveGameplayEffectAddedCallback(
+	UAbilitySystemComponent* Target, const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveHandle)
+{
+	FGameplayTagContainer AssetTags;
+	SpecApplied.GetAllAssetTags(AssetTags);
+
+	FGameplayTagContainer GrantedTags;
+	SpecApplied.GetAllGrantedTags(GrantedTags);
+
+	if (AssetTags.HasTagExact(EffectGameplayTag) || GrantedTags.HasTagExact(EffectGameplayTag))
+	{
+		ASComponent->OnGameplayEffectStackChangeDelegate(ActiveHandle)->AddUObject(this, &ThisClass::GameplayEffectStackChanged);
+		OnGameplayEffectStackChangeDelegate.Broadcast(EffectGameplayTag, ActiveHandle, 1, 0);
+	}
+}
+
+void UAsyncTask_ListenForGEStackChanged::OnRemoveGameplayEffectCallback(const FActiveGameplayEffect& EffectRemoved)
+{
+	FGameplayTagContainer AssetTags;
+	EffectRemoved.Spec.GetAllAssetTags(AssetTags);
+
+	FGameplayTagContainer GrantedTags;
+	EffectRemoved.Spec.GetAllGrantedTags(GrantedTags);
+
+	if (AssetTags.HasTagExact(EffectGameplayTag) || GrantedTags.HasTagExact(EffectGameplayTag))
+	{
+		OnGameplayEffectStackChangeDelegate.Broadcast(EffectGameplayTag, EffectRemoved.Handle, 0, 1);
+	}
+}
+
+void UAsyncTask_ListenForGEStackChanged::GameplayEffectStackChanged(FActiveGameplayEffectHandle EffectHandle, int32 NewStackCount, int32 PreviousStackCount)
+{
+	OnGameplayEffectStackChangeDelegate.Broadcast(EffectGameplayTag, EffectHandle, NewStackCount, PreviousStackCount);
+}
+
+void UAsyncTask_ListenForGEStackChanged::EndTask()
+{
+	if (IsValid(ASComponent))
+	{
+		ASComponent->OnActiveGameplayEffectAddedDelegateToSelf.RemoveAll(this);
+		ASComponent->OnAnyGameplayEffectRemovedDelegate().RemoveAll(this);
+	}
+	SetReadyToDestroy();
+	RemoveFromRoot();
+	MarkPendingKill();
+}
---
+++ Source/GAS_SR/Public/AsyncTask/AsyncTask_ListenForGEStackChanged.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
+#include "GameplayTagContainer.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "AsyncTask_ListenForGEStackChanged.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FOnGameplayEffectStackChangedSignature, FGameplayTag, EffectGameplayTag, FActiveGameplayEffectHandle, Handle, int32, NewStackCount, int32, OldStackCount);
+
+UCLASS(BlueprintType, meta=(ExposedAsyncProxy="AsyncTask"))
+class GAS_SR_API UAsyncTask_ListenForGEStackChanged : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+	/* Property */
+private:
+	static UAsyncTask_ListenForGEStackChanged* AsyncTask;
+	static UObject* WorldContextObject;
+	
+	UPROPERTY()
+	UAbilitySystemComponent* ASComponent;
+	FGameplayTag EffectGameplayTag;
+
+	UPROPERTY(BlueprintAssignable, DisplayName="OnGameplayEffectStackChange")
+	FOnGameplayEffectStackChangedSignature OnGameplayEffectStackChangeDelegate;
+
+	/* Function */
+public:
+	UFUNCTION(BlueprintCallable, Category="SR|AsyncTask", DisplayName="ListenForGEStackChanged_Async", meta=(HidePin="InWorldContextObject", DefaultToSelf="InWorldContextObject", BlueprintInternalUseOnly="true"))
+	static UAsyncTask_ListenForGEStackChanged* ListenForGEStackChanged(UObject* InWorldContextObject, UAbilitySystemComponent* InASComponent, FGameplayTag InEffectGameplayTag);
+	
+protected:
+	virtual void Activate() override;
+
+	virtual void OnActiveGameplayEffectAddedCallback(UAbilitySystemComponent* Target, const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveHandle);
+	virtual void OnRemoveGameplayEffectCallback(const FActiveGameplayEffect& EffectRemoved);
+	virtual void GameplayEffectStackChanged(FActiveGameplayEffectHandle EffectHandle, int32 NewStackCount, int32 PreviousStackCount);
+private:
+	UFUNCTION(BlueprintCallable, Category="SR|AsyncTask")
+	void EndTask();
+};
+UAsyncTask_ListenForGEStackChanged* UAsyncTask_ListenForGEStackChanged::AsyncTask = nullptr;
+UObject* UAsyncTask_ListenForGEStackChanged::WorldContextObject = nullptr;
