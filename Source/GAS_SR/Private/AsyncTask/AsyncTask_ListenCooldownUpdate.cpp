d18a4bd65ec897ae1165f6baada0bac499b41c1c
FHangH <752972182@qq.com>
2024-11-26 16:33:00 +0800

    Add: AsyncTask_ListenCooldownUpdate cpp file;

---
+++ Source/GAS_SR/Private/AsyncTask/AsyncTask_ListenCooldownUpdate.cpp
@@ -0,0 +1,135 @@
+﻿#include "AsyncTask/AsyncTask_ListenCooldownUpdate.h"
+#include "AbilitySystemComponent.h"
+#include "GameplayEffectTypes.h"
+
+UAsyncTask_ListenCooldownUpdate* UAsyncTask_ListenCooldownUpdate::ListenCooldownUpdateChanged(
+	UObject* InWorldContextObject, UAbilitySystemComponent* AbilitySystemComponent, const FGameplayTagContainer InCooldownTags, const bool bUseServerCooldown)
+{
+	AsyncTask = NewObject<UAsyncTask_ListenCooldownUpdate>();
+	if (AsyncTask)
+	{
+		AsyncTask->WorldContextObject = InWorldContextObject;
+		AsyncTask->ASComponent = AbilitySystemComponent;
+		AsyncTask->CooldownTags = InCooldownTags;
+		AsyncTask->IsUseServerCooldown = bUseServerCooldown;
+		return AsyncTask;
+	}
+	return nullptr;
+}
+
+void UAsyncTask_ListenCooldownUpdate::Activate()
+{
+	AddToRoot();
+	RegisterWithGameInstance(WorldContextObject);
+
+	if (!ASComponent || CooldownTags.Num() < 1)
+	{
+		EndTask();
+		return;
+	}
+	ASComponent->OnActiveGameplayEffectAddedDelegateToSelf.AddUObject(this, &ThisClass::OnActiveGameplayEffectAddedCallBack);
+
+	TArray<FGameplayTag> CooldownTagArray;
+	CooldownTags.GetGameplayTagArray(CooldownTagArray);
+	for (const auto& Tag : CooldownTagArray)
+	{
+		ASComponent->RegisterGameplayTagEvent(Tag, EGameplayTagEventType::NewOrRemoved).AddUObject(this, &ThisClass::OnCooldownTagChanged);
+	}
+}
+
+void UAsyncTask_ListenCooldownUpdate::OnActiveGameplayEffectAddedCallBack(
+	UAbilitySystemComponent* Target, const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveHandle)
+{
+	FGameplayTagContainer AssetTags;
+	SpecApplied.GetAllAssetTags(AssetTags);
+
+	FGameplayTagContainer GrantedTags;
+	SpecApplied.GetAllGrantedTags(GrantedTags);
+
+	TArray<FGameplayTag> CooldownTagArray;
+	CooldownTags.GetGameplayTagArray(CooldownTagArray);
+
+	for (const auto& CooldownTag : CooldownTagArray)
+	{
+		if (AssetTags.HasTagExact(CooldownTag) || GrantedTags.HasTagExact(CooldownTag))
+		{
+			auto TimeRemaining = 0.f;
+			auto CooldownDuration = 0.f;
+			
+			FGameplayTagContainer CooldownTagContainer {GrantedTags.GetByIndex(0)};
+			GetCooldownRemainingForTag(CooldownTagContainer, TimeRemaining, CooldownDuration);
+
+			if (ASComponent->GetOwnerRole() == ROLE_Authority)
+			{
+				OnCooldownBeginDelegate.Broadcast(CooldownTag, TimeRemaining, CooldownDuration);
+			}
+			else if (!IsUseServerCooldown && SpecApplied.GetContext().GetAbilityInstance_NotReplicated())
+			{
+				OnCooldownBeginDelegate.Broadcast(CooldownTag, TimeRemaining, CooldownDuration);
+			}
+			else if (IsUseServerCooldown && SpecApplied.GetContext().GetAbilityInstance_NotReplicated() == nullptr)
+			{
+				OnCooldownBeginDelegate.Broadcast(CooldownTag, TimeRemaining, CooldownDuration);
+			}
+			else if (IsUseServerCooldown && SpecApplied.GetContext().GetAbilityInstance_NotReplicated())
+			{
+				OnCooldownBeginDelegate.Broadcast(CooldownTag, -1.f, -1.f);
+			}
+		}
+	}
+}
+
+void UAsyncTask_ListenCooldownUpdate::OnCooldownTagChanged(const FGameplayTag CooldownTag, int32 NewCount)
+{
+	if (NewCount == 0)
+	{
+		OnCooldownEndDelegate.Broadcast(CooldownTag, -1.f, -1.f);
+	}
+}
+
+bool UAsyncTask_ListenCooldownUpdate::GetCooldownRemainingForTag(FGameplayTagContainer InCooldownTags, float& TimeRemaining, float& CooldownDuration) const
+{
+	if (ASComponent && InCooldownTags.Num() > 0)
+	{
+		TimeRemaining = 0.f;
+		CooldownDuration = 0.f;
+
+		const auto Query = FGameplayEffectQuery::MakeQuery_MatchAnyOwningTags(InCooldownTags);
+		auto DurationAndTimeRemaining = ASComponent->GetActiveEffectsTimeRemainingAndDuration(Query);
+
+		if (DurationAndTimeRemaining.Num() > 0)
+		{
+			auto BestIdx = 0;
+			auto LongestTime = DurationAndTimeRemaining[0].Key;
+			for (int32 Idx = 1; Idx < DurationAndTimeRemaining.Num(); ++Idx)
+			{
+				if (DurationAndTimeRemaining[Idx].Key > LongestTime)
+				{
+					LongestTime = DurationAndTimeRemaining[Idx].Key;
+					BestIdx = Idx;
+				}
+			}
+			TimeRemaining = DurationAndTimeRemaining[BestIdx].Key;
+			CooldownDuration = DurationAndTimeRemaining[BestIdx].Value;
+			return true;
+		}
+	}
+	return false;
+}
+
+void UAsyncTask_ListenCooldownUpdate::EndTask()
+{
+	if (ASComponent)
+	{
+		ASComponent->OnActiveGameplayEffectAddedDelegateToSelf.RemoveAll(this);
+		TArray<FGameplayTag> CooldownTagArray;
+		CooldownTags.GetGameplayTagArray(CooldownTagArray);
+		for (const auto& Tag : CooldownTagArray)
+		{
+			ASComponent->RegisterGameplayTagEvent(Tag, EGameplayTagEventType::NewOrRemoved).RemoveAll(this);
+		}
+	}
+	SetReadyToDestroy();
+	RemoveFromRoot();
+	MarkPendingKill();
+}
---
+++ Source/GAS_SR/Public/AsyncTask/AsyncTask_ListenCooldownUpdate.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "AsyncTask_ListenCooldownUpdate.generated.h"
+
+struct FGameplayEffectSpec;
+struct FActiveGameplayEffectHandle;
+class UAbilitySystemComponent;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnCooldownChangedSignature, FGameplayTag, CooldownTag, float, TimeRemaining, float, CooldownDuration);
+
+UCLASS(BlueprintType, meta=(ExposedAsyncProxy="AsyncTask"))
+class GAS_SR_API UAsyncTask_ListenCooldownUpdate : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+	/* Property */
+private:
+	static UAsyncTask_ListenCooldownUpdate* AsyncTask;
+	static UObject* WorldContextObject;
+	
+	UPROPERTY()
+	UAbilitySystemComponent* ASComponent {};
+	FGameplayTagContainer CooldownTags {};
+	bool IsUseServerCooldown { false };
+
+	UPROPERTY(BlueprintAssignable, DisplayName="CooldownBegin")
+	FOnCooldownChangedSignature OnCooldownBeginDelegate;
+
+	UPROPERTY(BlueprintAssignable, DisplayName="CooldownEnd")
+	FOnCooldownChangedSignature OnCooldownEndDelegate;
+	
+	/* Function */
+public:
+	UFUNCTION(BlueprintCallable, Category="SR|AsyncTask", DisplayName="ListenCooldownUpdateChanged_Async", meta=(HidePin="InWorldContextObject", DefaultToSelf="InWorldContextObject", BlueprintInternalUseOnly="true"))
+	static UAsyncTask_ListenCooldownUpdate* ListenCooldownUpdateChanged(UObject* InWorldContextObject, UAbilitySystemComponent* AbilitySystemComponent, FGameplayTagContainer InCooldownTags, bool bUseServerCooldown);
+
+protected:
+	virtual void Activate() override;
+
+	virtual void OnActiveGameplayEffectAddedCallBack(UAbilitySystemComponent* Target, const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveHandle);
+	virtual void OnCooldownTagChanged(const FGameplayTag CooldownTag, int32 NewCount);
+	bool GetCooldownRemainingForTag(FGameplayTagContainer InCooldownTags, float& TimeRemaining, float& CooldownDuration) const;
+	
+private:
+	UFUNCTION(BlueprintCallable, Category="SR|AsyncTask")
+	void EndTask();
+};
+UAsyncTask_ListenCooldownUpdate* UAsyncTask_ListenCooldownUpdate::AsyncTask = nullptr;
+UObject* UAsyncTask_ListenCooldownUpdate::WorldContextObject = nullptr;
