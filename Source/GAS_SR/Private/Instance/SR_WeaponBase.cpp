c98429a35f4573f46971d695a89133dfa8b9a4ef
FHangH <752972182@qq.com>
2024-11-28 16:35:46 +0800

    Add: SR_WeaponBase cpp file;

---
+++ Source/GAS_SR/Private/Instance/SR_WeaponBase.cpp
@@ -0,0 +1,23 @@
+﻿#include "Instance/SR_WeaponBase.h"
+#include "Components/CapsuleComponent.h"
+
+ASR_WeaponBase::ASR_WeaponBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	TriggerBoxComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("TriggerBoxComponent"));
+	TriggerBoxComponent->SetupAttachment(EquipMeshComponent);
+}
+
+void ASR_WeaponBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASR_WeaponBase::BeginWeaponAttack_Implementation(const FGameplayTag& Tag)
+{
+}
+
+void ASR_WeaponBase::EndWeaponAttack_Implementation()
+{
+}
---
+++ Source/GAS_SR/Public/Instance/SR_WeaponBase.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "SR_EquipmentBase.h"
+#include "GameplayTagContainer.h"
+#include "SR_WeaponBase.generated.h"
+
+class UCapsuleComponent;
+
+UCLASS()
+class GAS_SR_API ASR_WeaponBase : public ASR_EquipmentBase
+{
+	GENERATED_BODY()
+
+	/* Property */
+public:
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category="A_SR|Weapon")
+	UCapsuleComponent* TriggerBoxComponent;
+
+protected:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="A_SR|Weapon")
+	FGameplayTag CurrentEventTag;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="A_SR|Weapon")
+	bool IsAttacking = false;
+	
+	/* Function */
+public:
+	ASR_WeaponBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="A_SR|Weapon", meta=(AutoCreateRefTerm="Tag"))
+	void BeginWeaponAttack(const FGameplayTag& Tag);
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="A_SR|Weapon")
+	void EndWeaponAttack();
+};
